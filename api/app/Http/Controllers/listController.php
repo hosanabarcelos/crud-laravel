a32bdff187842c59d67b204a5271fa40759508af
Hosana <hosanabarceloscandido@gmail.com>
2023-05-28 14:09:36 -0300

    feat: create controller with crud

---
+++ api/app/Http/Controllers/listController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ListItem;
+use Illuminate\Http\Request;
+
+class listController extends Controller
+{
+    //GET
+    public function listing(Request $request)
+    {
+        $ListItem = ListItem::all();
+
+        return response()->json($ListItem);
+    }
+
+    //POST
+    public function create(Request $request)
+    {
+        $newPhrase = new ListItem();
+        $newPhrase->category = $request->category;
+        $newPhrase->phrase = $request->phrase;
+        $newPhrase-> save();
+
+        return response()->json('success');
+    }
+
+    //GET phrase
+    public function edit(ListItem $phrase, Request $request)
+    {
+        return response()->json($phrase);
+    }
+
+    //PUT
+    public function update(ListItem $phrase, Request $request)
+    {
+        $phrase->category = $request->category;
+        $phrase->phrase = $request->phrase;
+        $phrase->save();
+
+        return response()->json('success');
+    }
+
+    public function delete(ListItem $phrase, Request $request)
+    {
+        $phrase->delete();
+
+        return response()->json('success');
+    }
+}
